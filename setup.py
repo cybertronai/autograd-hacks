f840529e223e1fb399ec4d99bfe9f074a948b280
Justus Schwabedal <jschwabedal@gmail.com>
2020-02-28 17:38:45 +0100

    Add reqs to setup.py using setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 
-from distutils.core import setup
+from setuptools import setup
+
+def requirements():
+    reqs = []
+    with open('requirements.txt', 'r') as fp:
+        for req in fp:
+            # remove endline, white space, and anything after '#'
+            req = req.rstrip('\n').strip().split('#')[0]
+            if req is '':
+                continue
+
+            reqs.append(req)
+
+    return reqs
 
 setup(
     name='autograd_hacks',
     version='0.0.2',
     packages=['autograd_hacks'],
     long_description=open('README.md').read(),
+    install_requires=requirements()
 )
